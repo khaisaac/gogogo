abce5e75e9e1548f179a7559fe9fb10c4a2e002b
khaisaac <khaisarrizkimaulana@unbaja.ac.id.com>
2026-05-27 17:44:37 +0700

    Update TripAdvisor widget code

--- src/components/TripAdvisorWidget.tsx
+++ src/components/TripAdvisorWidget.tsx
@@ -1,32 +1,20 @@
 "use client";
-import { useEffect, useRef } from 'react';
 
 export default function TripAdvisorWidget() {
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (!containerRef.current) return;
-    
-    // Check if script is already injected in this container
-    if (containerRef.current.querySelector('script')) return;
-
-    const script = document.createElement('script');
-    script.src = "https://www.jscache.com/wejs?wtype=selfserveprop&uniq=726&locationId=10755003&lang=en_US&rating=true&nreviews=5&writereviewlink=true&popIdx=false&iswide=false&border=false&display_version=2";
-    script.async = true;
-    containerRef.current.appendChild(script);
-  }, []);
-
   return (
     <div className="tripadvisor-widget-container" style={{ marginTop: '24px' }}>
-      <div ref={containerRef} id="TA_selfserveprop726" className="TA_selfserveprop">
-        <ul id="Pe1XyuZT" className="TA_links nSfL8Pjb">
-          <li id="HaaVH0bX" className="PTsTj2ung7">
-            <a target="_blank" href="https://www.tripadvisor.com/" rel="noopener noreferrer">
-              <img src="https://www.tripadvisor.com/img/cdsi/img2/branding/150_logo-11900-2.png" alt="TripAdvisor"/>
-            </a>
-          </li>
-        </ul>
-      </div>
+      <iframe 
+        src="/tripadvisor-widget.html" 
+        style={{ 
+          border: 'none', 
+          width: '100%', 
+          maxWidth: '300px',
+          height: '150px', 
+          overflow: 'hidden' 
+        }}
+        title="TripAdvisor Reviews"
+        scrolling="no"
+      />
     </div>
   );
 }
